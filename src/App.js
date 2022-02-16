95733fb5315ce7bb2928b932e95b796fea4d1250
mawrkus <web@sparring-partner.be>
2022-02-16 01:56:07 +0100

    Created FilterBar component

--- src/App.js
+++ src/App.js
@@ -2,83 +2,13 @@
 import { Form } from 'semantic-ui-react';
 
 import './App.css';
+import { FilterBar } from './FilterBar';
 
-import { Chiclet } from './components/Chiclet';
-import { PartialChiclet } from './components/PartialChiclet';
-import { SuggestionsDropdown } from './components/SuggestionsDropdown';
-
-import { entityStateMachine } from './entity-state/entityStateMachine';
-import { useStateMachine } from './entity-state/useStateMachine';
-
-function App() {
-  const [props] = useStateMachine(entityStateMachine);
-
-  const onOpenSuggestionsDropdown = () => {
-    entityStateMachine.sendEvent("startInput");
-  };
-
-  const onCloseSuggestionsDropdown = () => {
-    entityStateMachine.sendEvent("discardSuggestions");
-  };
-
-  const onSelectSuggestionItem = (event, item) => {
-    entityStateMachine.sendEvent("selectItem", item);
-  };
-
-  const onRemoveChiclet = (event, filter) => {
-    entityStateMachine.sendEvent("removeFilter", filter);
-  };
-
-  const onClickChiclet = (event, filter, part) => {
-    if (part === 'operator') {
-      return entityStateMachine.sendEvent("editOperatorSuggestion", { filter });
-    }
-
-    if (part === 'value') {
-      return entityStateMachine.sendEvent("editValueSuggestion", { filter });
-    }
-
-    if (part === 'logical-operator') {
-      return entityStateMachine.sendEvent("editLogicalOperatorSuggestion", { filter });
-    }
-  };
-
-  const onClickPartialChiclet = (event, filter, part) => {
-    if (part === 'operator') {
-      return entityStateMachine.sendEvent("editPartialOperatorSuggestion", { filter });
-    }
-  };
-
+export const App = () => {
   return (
     <div className="container">
       <h2 className="title">🧪 Rick &amp; Morty filter bar 🧪</h2>
-
-      <Form>
-        <Form.Group>
-          {props.filters.map((filter) => (
-            <Chiclet
-              key={filter.id}
-              filter={filter}
-              onClick={onClickChiclet}
-              onRemove={onRemoveChiclet}
-            />
-          ))}
-
-          <PartialChiclet
-            filter={props.partialFilter}
-            onClick={onClickPartialChiclet}
-          />
-
-          <SuggestionsDropdown
-            open={props.isDisplayed}
-            loading={props.isLoading}
-            suggestions={props.suggestions}
-            onOpen={onOpenSuggestionsDropdown}
-            onClose={onCloseSuggestionsDropdown}
-            onSelectItem={onSelectSuggestionItem}
-          />
-        </Form.Group>
-      </Form>
+      <FilterBar />
     </div>
   );
 }

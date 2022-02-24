4c5f3a6e688a49cf68ca5e6272e6d357f14f75e2
mawrkus <web@sparring-partner.be>
2022-02-24 01:27:51 +0100

    Moved init filters to new file

--- src/App.js
+++ src/App.js
@@ -4,35 +4,8 @@ import { FilterBar } from "./components/FilterBar";
 import { buildAppStateMachine } from "./state";
 import { useMemo, useState } from "react";
 
+// import { initFilters } from "./initFilters";
 const initFilters = [];
-
-// const initFilters = [
-//   {
-//     id: 1,
-//     attribute: { id: 2, value: "season", label: "Season" },
-//     operator: { id: 2, value: "!=", label: "!=" },
-//     value: { id: 118549, value: 5, label: "5 (10 episodes)" },
-//     type: "attribute-operator-value",
-//   },
-//   {
-//     id: 2,
-//     attribute: null,
-//     operator: { id: 1, value: "and", label: "AND" },
-//     value: null,
-//     type: "logical-operator",
-//   },
-//   {
-//     id: 3,
-//     attribute: { id: 3, value: "episode", label: "Episode" },
-//     operator: { id: 1, value: "=", label: "=" },
-//     value: {
-//       id: 2127407,
-//       value: "Rick & Morty's Thanksploitation Spectacular",
-//       label: "Rick & Morty's Thanksploitation Spectacular (S5E6)",
-//     },
-//     type: "attribute-operator-value",
-//   },
-// ];
 
 export const App = () => {
   const [currentFilters, setCurrentFilters] = useState(initFilters);

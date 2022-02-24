94755285b20c4397e15b00bc4c080d177f516633
mawrkus <web@sparring-partner.be>
2022-02-24 02:10:08 +0100

    Fixed bug editing partial filter operator to is (not) null

--- src/state/states/operatorSuggestions.js
+++ src/state/states/operatorSuggestions.js
@@ -70,12 +70,32 @@ export const editOperator = {
     discardSuggestions: "idle",
     selectItem: [
       {
-        cond: (event, ctx) => typeof ctx.get().filterUnderEdition.operator.presetValue !== 'undefined' && typeof event.data.presetValue === 'undefined',
+        // e.g.: IS NULL -> =
+        cond: (event, ctx) => typeof ctx.get().filterUnderEdition?.operator.presetValue !== 'undefined'
+          && typeof event.data.presetValue === 'undefined',
         targetId: "loadValueSuggestions",
         action:(event, ctx) => {
           const { filterUnderEdition } = ctx.get();
+
           ctx.setFilterOperator(event.data);
           ctx.startEditing(filterUnderEdition);
+        },
+      },
+      // partial filter edition, e.g.: = -> IS NULL
+      {
+        cond: (event, ctx) => typeof ctx.get().partialFilter.operator?.presetValue === 'undefined'
+          && typeof event.data.presetValue !== 'undefined',
+        targetId: "idle",
+        action:(event, ctx) => {
+          ctx.setFilterOperator(event.data);
+
+          const filterValue = {
+            id: null,
+            value: event.data.presetValue,
+            label: String(event.data.presetValue),
+          };
+
+          ctx.completePartialFilter(filterValue, 'attribute-operator');
         },
       },
       {

2a3cc4b91c9fab51a3e66f8832e68d0e5215f0f0
mawrkus <web@sparring-partner.be>
2022-02-27 02:18:40 +0100

    ...

--- src/AppMessage.js
+++ src/AppMessage.js
@@ -4,7 +4,7 @@ import { Message } from "semantic-ui-react";
 export const AppMessage = ({ stateId, filters }) => {
   return (
     <Message size="mini" style={{ marginTop: '24px', boxShadow: 'none' }}>
-      <Message.Header>Filters in state "{stateId}" ({filters.length})</Message.Header>
+      <Message.Header>Filters in "{stateId}" state ({filters.length})</Message.Header>
       <Message.List as="ol">
         {filters.map((filter) => (
           <Message.Item key={filter.id}>{JSON.stringify(filter)}</Message.Item>

c7d937f3d7913a671946832419e8e37e08e82ff2
mawrkus <web@sparring-partner.be>
2022-03-06 12:48:11 +0100

    Added button to generate URL with current filters

--- src/AppMessage.js
+++ src/AppMessage.js
@@ -1,10 +1,26 @@
 import { memo } from "react";
-import { Message } from "semantic-ui-react";
+import { Message, Button } from "semantic-ui-react";
 
 export const AppMessage = ({ stateId, filters }) => {
   return (
-    <Message size="mini" style={{ marginTop: '24px', boxShadow: 'none' }}>
-      <Message.Header>Filters in "{stateId}" state ({filters.length})</Message.Header>
+    <Message size="tiny" style={{ marginTop: "24px", boxShadow: "none" }}>
+      <Message.Header style={{ display: "flex" }}>
+        <span>
+          Filters in "{stateId}" state ({filters.length})
+        </span>
+
+        <Button
+          as="a"
+          target="_blank"
+          circular
+          icon="linkify"
+          size="mini"
+          style={{ marginLeft: "10px", fontSize: "7px" }}
+          title="Open in new tab"
+          href={`/?filters=${encodeURIComponent(JSON.stringify(filters))}`}
+        />
+      </Message.Header>
+
       <Message.List as="ol">
         {filters.map((filter) => (
           <Message.Item key={filter.id}>{JSON.stringify(filter)}</Message.Item>

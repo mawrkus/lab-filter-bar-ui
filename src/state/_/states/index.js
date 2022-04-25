dec3ae98db95d54d1618ba4d59a4ac16bfbbea43
mawrkus <web@sparring-partner.be>
2022-04-25 21:30:02 +0200

    Removed unused code (previous state machine)

--- src/state/_/states/index.js
+++
@@ -1,7 +0,0 @@
-export * from './idle';
-export * from './attributeSuggestions';
-export * from './operatorSuggestions';
-export * from './valueSuggestions';
-export * from './logicalOperatorSuggestions';
-export * from './displayPartialFilterSuggestions';
-export * from './displayParensSuggestions';
